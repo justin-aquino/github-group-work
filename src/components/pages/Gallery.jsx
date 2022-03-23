cee680c3f40cd4bbaf30954021359391fb4c5179
justin-aquino <justin.aquino25@gmail.com>
2022-03-23 16:44:56 -0700

    adding Gallery,jsx

---
+++ src/components/pages/Gallery.jsx
@@ -0,0 +1,15 @@
+
+
+const Gallery = () => {
+  return (
+    <div>
+        <img src="https://assets-global.website-files.com/5f3c19f18169b62a0d0bf387/60d33be8cf4ba7565123c8bc_YPD3ulQQAGQpOcnqIm3QzSTRgzmr1SexpW9ZjMpJ1mAnUxx4iF05XOTu44sk0qQG-8XgBcYmGZGAD-5SAZvJl3TjtmhgWnn-w0C2XKwhBscV78RVvhwZfyp0v_Pa6sNj5zxpOvRW.png" alt="meme1"/>
+        <img src="https://i.pinimg.com/736x/ac/b7/f9/acb7f99ba0ef473e03ee81e91a6281ee.jpg" alt="meme2" />
+        <img src="https://preview.redd.it/54ss55ix0vwy.jpg?auto=webp&s=e804cd7d44dfe7dd1c4405533c2ba3fd41fa971e" alt="meme3" />
+        <img src="https://pbs.twimg.com/media/EXUgmSsXQAAlWg7.jpg" alt="meme4" />
+        <img src="https://aws1.discourse-cdn.com/standard14/uploads/daml/optimized/2X/6/67f84d7cc57fd16c74e4d9f0df50190748848c7b_2_414x500.jpeg" alt="meme5" />
+    </div>
+  )
+}
+
+export default Gallery
